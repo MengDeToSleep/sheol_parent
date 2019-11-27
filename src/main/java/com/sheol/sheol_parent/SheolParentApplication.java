4b55aa12b4b68a2764b65f2fa95179015c0e615c
xiao <827549003@qq.com>
2019-11-27 11:46:59 +0800

    sheolCRM

---
+++ src/main/java/com/sheol/sheol_parent/SheolParentApplication.java
@@ -0,0 +1,13 @@
+package com.sheol.sheol_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheolParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheolParentApplication.class, args);
+    }
+
+}
